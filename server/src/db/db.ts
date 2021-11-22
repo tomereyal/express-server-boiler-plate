ba78f8b90e6a67369bc10338699348fb468634a6
Tomer Eyal <tomereyal@gmail.com>
2021-11-22 10:41:53 +0200

    Final

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -4,6 +4,6 @@ export const db = mysql2.createPool({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) | 3306,
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "mrtom45",
-  database: "trips",
+  password: process.env.DB_PASS || "",
+  database: "",
 });
